5100ec1b87f79f29a5819b8ada927c7380eb082d
gr426_krnmi <nik.kravczik.2006@mail.ru>
2025-02-06 13:15:46 +0700

    pro3

--- js/main.js
+++ js/main.js
@@ -1,48 +1,8 @@
-// let product = "Socks";
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-        ],
-        cart: 0
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        }
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        description: "A pair of warm, fuzzy socks."
     }
 })
-
-
