cfdc790ae0c4a6515e8447346cfe18603700b9f0
gr426_krnmi <nik.kravczik.2006@mail.ru>
2025-02-06 13:56:40 +0700

    pro4

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         description: "A pair of warm, fuzzy socks.",
         altText: "A pair of socks",
+        inStock: true,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
+        inventory: 100,
+        onSale: "On Sale "
+
     }
 })
 
