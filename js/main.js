c7608b84e7125ccbe01d04430b92a7cf033a75d5
Никита Кравцов <gr426_krnmi@lab-308-3.tomtit-tomsk.ru>
2025-02-06 10:55:08 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// let product = "Socks";
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+
+    }
+})
