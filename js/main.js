de037c2dfad51f0db58f6d54faaeb78b9dc971d0
gr426_krnmi <nik.kravczik.2006@mail.ru>
2025-02-06 13:46:27 +0700

    pro3

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         description: "A pair of warm, fuzzy socks.",
         altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
     }
 })
 
