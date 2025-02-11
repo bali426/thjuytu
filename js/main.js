90d4f0da8c3f3d131e1dde4698f9457d64f7452c
public <nik.kravczik.2006@mail.ru>
2025-02-11 17:46:30 +0700

    kra2

--- js/main.js
+++ js/main.js
@@ -3,29 +3,31 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         image: "./assets/vmSocks-green-onWhite.jpg",
         description: "A pair of warm, fuzzy socks.",
         altText: "A pair of socks",
-        inStock: false,
+        selectedVariant: 0,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
         inventory: 100,
-        onSale: "On Sale ",
+        onSale: true,
+        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
-
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-
+                variantQuantity: 0
             }
-
         ],
+
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
     },
@@ -42,8 +44,18 @@ let app = new Vue({
 
 
 
-    }
 
-})
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        sale() {
+            return this.onSale
+                ? `Сейчас распродажа на ${this.brand} ${this.product}!`
+                : `Распродажа не проводится на ${this.brand} ${this.product}.`;
+        }
+    }
+});
 
 
